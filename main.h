72e8b5694fb72a00f53bce216a032e40a87e7210
Giann Pabon <giannalberto2@outlook.com>
2024-12-01 06:56:21 -0400

    upload main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int specifier_cases(char specifier, va_list args);
+int string(char *str);
+
+#endif
+
