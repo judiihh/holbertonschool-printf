12ea232c6adea6f925b26db69e9f1ee8e84db515
Giann Pabon <giannalberto2@outlook.com>
2024-12-01 07:02:05 -0400

    Specificer cases

---
+++ specifier_cases.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * specifier_cases - Handles the specifiers for the _printf function.
+ *
+ * @specifier: The character specifying the format type.
+ * @args: Argument list.
+ *
+ * Return: Number of characters printed.
+ */
+
+int specifier_cases(char specifier, va_list args)
+{
+	switch (specifier)
+	{
+		case 'c':
+			{
+				char c = va_arg(args, int);
+
+				return (write(1, &c, 1));
+			}
+		case 's':
+			return (string(va_arg(args, char *)));
+		case '%':
+			return (write(1, "%", 1));
+
+		default:
+			{
+				char invalid[] = {'%', specifier};
+
+				return (write(1, invalid, 2));
+			}
+	}
+}
+
